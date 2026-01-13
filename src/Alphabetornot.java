02093c332c72bc9e1cb057d3ed921ca5a491d34c
PavanAzmeeraSIDGloba <pavanazmeera1@gmail.com>
2026-01-13 14:46:50 +0530

    if condition

--- src/Alphabetornot.java
+++ src/Alphabetornot.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Alphabetornot {
-    public static  void main(String arg[]){
-        Scanner sc = new Scanner (System.in);
+    public static void main (String arg[]){
+        Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
 
-        if (ch=="a"){
-
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')){
+            System.out.println(ch + " is an Alphabet");
+        } else {
+            System.out.println(ch + " is NOT an Alphabet");
         }
     }
 }
--- src/Vowelorconsonant.java
+++ src/Vowelorconsonant.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class Vowelorconsonant {
     public static void main (String arg[]){
-        System.out.print("Enter a character: ");
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
         // Convert to lowercase to handle both uppercase & lowercase
         ch = Character.toLowerCase(ch);
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-            System.out.println("you entered the ovel :"+ch);
+            System.out.println("you entered the ovel"+ch);
 
         }else{
-            System.out.println("you entered the is not ovel :"+ch);
+            System.out.println("you entered the is not ovel "+ch);
         }
     }
 
